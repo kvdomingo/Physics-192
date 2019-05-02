c02b2a7b30d76691260fc64567174981c04cfda2
Kenneth Domingo <kvdomingo@up.edu.ph>
2019-05-03 01:36:16 +0800

    C3 data

--- C1/C1data.m
+++ C1/C1data.m
@@ -1,4 +1,4 @@
-dat = csvread("openend2.csv", 2);
+dat = csvread("50ohm2.csv", 2);
 t = dat(:,1);
 Vin = dat(:,2);
 Vout = dat(:,3);
@@ -12,7 +12,6 @@ xlabel("time (\mus)")
 ylabel("amplitude (V)")
 legend("V_{in}", "V_{out}")
 
-dt = (4.74 - 3.73)*1e-6;
+dt = (3.52 - 3.27)*1e-6;
 L = 49;
 c = 2*L/dt;
-c
